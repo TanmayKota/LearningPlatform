f01e73289acbcedb6c710ed6cb3656135611151e
Tanmay Kota <120029379+TanmayKota@users.noreply.github.com>
2025-10-27 21:48:24 +0100

    Update build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-dotnet add package Microsoft.AspNetCore.Components.WebAssembly --version <version> \
-    && dotnet add package Microsoft.AspNetCore.Components.WebAssembly.DevServer --version <version> \
-    && dotnet add package Microsoft.Extensions.Configuration.Binder --version <version>
+dotnet add package Microsoft.AspNetCore.Components.WebAssembly --version 8.0.0 && \
+dotnet add package Microsoft.AspNetCore.Components.WebAssembly.DevServer --version 8.0.0 && \
+dotnet add package Microsoft.Extensions.Configuration.Binder --version 8.0.0
