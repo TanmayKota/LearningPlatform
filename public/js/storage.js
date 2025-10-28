d3d9872f486ea4fa01b6d74b984f6a6d6cfd0792
Tanmay Kota <120029379+TanmayKota@users.noreply.github.com>
2025-10-28 10:36:47 +0100

    vercel-deploy

---
+++ public/js/storage.js
@@ -0,0 +1,5 @@
+﻿window.appStorage = {
+    set: function (k, v) { localStorage.setItem(k, v); },
+    get: function (k) { return localStorage.getItem(k); },
+    remove: function (k) { localStorage.removeItem(k); }
+};
